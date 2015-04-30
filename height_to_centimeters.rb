5432b0b13a46a4408001a5013b8d8d7e9e3b1402
alfonso-cabrera <a.l.cabrera11@gmail.com>
2015-04-30 00:06:41 +0000

    madlibs

--- height_to_centimeters.rb
+++ height_to_centimeters.rb
@@ -1,4 +1,9 @@
-puts "Hello! What is your height in inches?"
-x = gets.chomp 
-y = x.to_i * 2.54
-puts "Congratulations!  You are " + y.to_s + " centimeters tall!"
+height = print "Hi friend! What is your height in inches?"
+puts height.is_a? Integer
+if height.is_a? Numeric
+  convert_to_centimeters = height.to_i * 2.54
+  puts "Congratulations!  You are #{convert_to_centimeters} centimeters tall!"
+else height.is_a? Numeric
+  print "Please only enter your height as a number (in inches!)"
+end
+
--- madlib.rb
+++ madlib.rb
@@ -46,24 +46,11 @@ puts "Welcome to the nursery rhymes madlib " + user_name + "!"
   celestial_body = gets.chomp
 
 # ok, we have all the input from the user, now print out the results:
-  puts "Thanks " + user_name  + "!  Here's your personalized version of 'Mary had a Little Lamb'..."
+puts "Thanks #{user_name} !  Here's your madlib!..."
   puts ""
-  puts famous_person + " had a little " + animal + ","
-  puts "it's " + body_part + " was " + color + " as " + food + "."
-  puts "and everywhere that " + famous_person + " went "
-  puts "the " + animal + " was sure to go!"
-  puts ""
-# make the user press any key to get the second mad lib:
-  puts "That was great, wasn't it?  Let's do another one! Hit Enter when you're ready!"
-  gets
-  puts ""
-  puts "Thanks again " + user_name  + "!  Here's your personalized version of 'The Itsy-Bitsy Spider'..."
-  puts ""
-  puts "The itsy-bitsy " + animal + " went up the " + place + " spout."
-  puts "Down came the " + weather + ", and " + action_verb + "ed the " + animal + " out."
-  puts "Out came the " + celestial_body + ", and dried up all the " + weather + ", and"
-  puts "The itsy-bitsy " + animal + " went up the " + place + " spout again!"
-  puts ""
+  puts "#{user_name} had a little #{animal} named #{famous_person},"
+  puts "it's #{food} was #{color} #{body_part}."
+puts "they lived in #{place} until the Great #{weather} of 2015 came."
+puts "then they were #{action_verb} to #{celestial_body}!"
 
-puts " "
 puts "Thanks for playing!"
