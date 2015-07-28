d062953991c8c17004bc6452b8b51c30ed3c86ba
Alfonso Cabrera <a.l.cabrera11@gmail.com>
2015-07-28 09:47:45 -0400

    Delete height_to_centimeters.rb

--- height_to_centimeters.rb
+++
@@ -1,9 +0,0 @@
-height = print "Hi friend! What is your height in inches?"
-puts height.is_a? Integer
-if height.is_a? Numeric
-  convert_to_centimeters = height.to_i * 2.54
-  puts "Congratulations!  You are #{convert_to_centimeters} centimeters tall!"
-else height.is_a? Numeric
-  print "Please only enter your height as a number (in inches!)"
-end
-
